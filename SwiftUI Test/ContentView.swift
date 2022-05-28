d789b269653403108eaf7c2f397e53d3f8293a00
Kelvin Tam <3705865+kelvinthh@users.noreply.github.com>
2022-05-28 21:06:55 +0800

    Initial Commit

---
+++ SwiftUI Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Test
+//
+//  Created by Kelvin Tam on 28/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Test/SwiftUI_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TestApp.swift
+//  SwiftUI Test
+//
+//  Created by Kelvin Tam on 28/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
