455092eb8777495a8b645049845e7d4605be0d90
Kelvin Tam <3705865+kelvinthh@users.noreply.github.com>
2022-05-28 22:39:48 +0800

    Nav View

--- SwiftUI Test/ContentView.swift
+++ SwiftUI Test/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isActive = false
     var body: some View {
-        Text("Hello, world!")
+        NavigationView{
+            VStack(alignment: .center){
+                
+                CardView(isActive: self.$isActive)
+                CardView(isActive: self.$isActive)
+                CardView(isActive: self.$isActive)
+                Divider()
+                Spacer()
+                Button(action: {isActive = true}) {
+                    Text("Lovely Button")
+                        .foregroundColor(Color.orange)
+                }
+            }
             .padding()
+            .background(Color.blue)
+            .navigationTitle(
+                Text("Swift UI Test 💻")
+            )
+        }
+        
+    }
+}
+
+struct CardView: View {
+    @Binding var isActive: Bool
+    var body: some View{
+        VStack(alignment: .leading, spacing: 0.0){
+            Text("Hello World")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("Don't come to my house")
+            Text("I will quickscope your mom with my CoD")
+        }
+        .padding(.all, 15.0)
+        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.white/*@END_MENU_TOKEN@*/)
+        .cornerRadius(/*@START_MENU_TOKEN@*/15.0/*@END_MENU_TOKEN@*/)
+        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+            .previewInterfaceOrientation(.portrait)
     }
 }
